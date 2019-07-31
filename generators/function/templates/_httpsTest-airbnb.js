04884660ccbd4b2bd5b7a2a88080d39474f4e32b
Scott Prue <scott@residebrokerage.com>
2019-07-30 20:39:25 -0400

    Major update + Added example
    feat(function): update function generator and templates from generator-react-firebase - #3

--- generators/function/templates/_httpsTest-airbnb.js
+++ generators/function/templates/_httpsTest-airbnb.js
@@ -1,19 +1,19 @@
-<% if (eventType === 'onCall') { %>import { to } from 'utils/async'
+<% if (eventType === 'onCall') { %>import { to } from 'utils/async';
 
 describe('<%= camelName %> HTTPS Callable Cloud Function', () => {
-  let <%= name %>
-  let configStub
-  let adminInitStub
-  let functions
-  let admin
+  let <%= name %>;
+  let configStub;
+  let adminInitStub;
+  let functions;
+  let admin;
 
   before(() => {
     /* eslint-disable global-require */
-    admin = require('firebase-admin')
+    admin = require('firebase-admin');
     // Stub Firebase's admin.initializeApp
-    adminInitStub = sinon.stub(admin, 'initializeApp')
+    adminInitStub = sinon.stub(admin, 'initializeApp');
     // Stub Firebase's functions.config()
-    functions = require('firebase-functions')
+    functions = require('firebase-functions');
     configStub = sinon.stub(functions, 'config').returns({
       firebase: {
         databaseURL: 'https://not-a-project.firebaseio.com',
@@ -22,73 +22,71 @@ describe('<%= camelName %> HTTPS Callable Cloud Function', () => {
         messagingSenderId: '823357791673',
       },
       // Stub any other config values needed by your functions here
-    })
-    <%= name %> = require(`./index`).<%= name %>Request
+    });
+    <%= name %> = require(`./index`).<%= name %>Request;
     /* eslint-enable global-require */
-  })
+  });
 
   after(() => {
     // Restoring our stubs to the original methods.
-    configStub.restore()
-    adminInitStub.restore()
-  })
+    configStub.restore();
+    adminInitStub.restore();
+  });
 
   it('responds with hello message when sent an empty request', async () => {
-    const data = {}
-    const context = {}
+    const data = {};
+    const context = {};
     // Invoke request handler with fake data + context objects
-    const [err, response] = await to(<%= name %>(data, context))
+    const [err, response] = await to(<%= name %>(data, context));
     // Confirm no error is thrown
     expect(err).to.not.exist
     // Confirm response contains message
-    expect(response).to.have.property('message', 'Hello World')
-  })
-})<% } %><% if (eventType !== 'onCall') { %>describe('<%= camelName %> HTTPS Cloud Function', () => {
-  let myFunctions
-  let configStub
-  let adminInitStub
-  let functions
-  let admin
+    expect(response).to.have.property('message', 'Hello World');
+  });
+});<% } %><% if (eventType !== 'onCall') { %>describe('<%= camelName %> HTTPS Cloud Function', () => {
+  let myFunctions;
+  let configStub;
+  let adminInitStub;
+  let functions;
+  let admin;
 
   before(() => {
     /* eslint-disable global-require */
-    admin = require('firebase-admin')
+    admin = require('firebase-admin');
     // Stub Firebase's admin.initializeApp
-    adminInitStub = sinon.stub(admin, 'initializeApp')
+    adminInitStub = sinon.stub(admin, 'initializeApp');
     // Stub Firebase's functions.config()
-    functions = require('firebase-functions')
+    functions = require('firebase-functions');
     configStub = sinon.stub(functions, 'config').returns({
       firebase: {
         databaseURL: 'https://not-a-project.firebaseio.com',
         storageBucket: 'not-a-project.appspot.com',
         projectId: 'not-a-project.appspot',
-        messagingSenderId: '823357791673'
-      }
+        messagingSenderId: '823357791673',
+      },
       // Stub any other config values needed by your functions here
-    })
-    myFunctions = require(`../../index`)
+    });
+    myFunctions = require(`../../index`);
     /* eslint-enable global-require */
-  })
+  });
 
   after(() => {
     // Restoring our stubs to the original methods.
-    configStub.restore()
-    adminInitStub.restore()
-  })
+    configStub.restore();
+    adminInitStub.restore();
+  });
 
-  it('responds with hello message when sent an empty request', done => {
-    const req = {}
-    // Parameters of request can also be stubbed
-    // const req = { query: { text: 'input' } }
+  it('responds with hello message when sent an empty request', (done) => {
+    const req = {};
     // A fake response object, with a stubbed end function which asserts that
     // it is called with a hello message
     const res = {
       end: (msg) => {
-        expect(msg).to.equal('Hello from <%= name %>!')
-        done()
-      }
-    }
+        expect(msg).to.equal('Hello from <%= name %>');
+        done();
+      },
+    };
     // Invoke https function with fake request + response objects
-    myFunctions.<%= camelName %>(req, res)
-  })
-})<% } %>
+    myFunctions.<%= name %> (req, res);
+  });
+});<% } %>
---
+++ generators/function/templates/_pubsubFunction.js
@@ -0,0 +1,72 @@
+import * as functions from 'firebase-functions'
+<% if (eventType === 'onPublish') { %>
+/**
+ * Parse message body from message into JSON handling errors
+ * @param {Object} message - Message object from pubsub containing json
+ * parameter with body of message
+ * @return {Object}
+ */
+function parseMessageBody(message) {
+  try {
+    return message.json
+  } catch (e) {
+    console.error('PubSub message was not JSON and an error was thrown: ', e)
+    return null
+  }
+}
+
+/**
+ * @param {functions.pubsub.Message} message - Pubsub message object
+ * @return {Promise}
+ */
+async function <%= camelName %>Event(message) {
+  // Parse message body from message into JSON
+  const messageBody = parseMessageBody(message)
+
+  // Handle message not having a body
+  if (!messageBody) {
+    const noBodyMsg = 'The message does not have a body'
+    console.error(noBodyMsg)
+    throw new Error(noBodyMsg)
+  }
+
+  // Get attributes from message
+  const { attributes } = message || {}
+
+  console.log('Pub Sub message: ', { messageBody, attributes })
+
+  // End function execution by returning
+  return null
+}
+
+/**
+ * Cloud Function triggered by a PubSub message publish
+ *
+ * Trigger: `PubSub - onPublish`
+ * @name <%= camelName %>
+ * @type {functions.CloudFunction}
+ */
+export default functions.pubsub
+  .topic('<%= camelName %>')
+  .onPublish(<%= camelName %>Event)<% } else { %>
+/**
+ * @param {functions.pubsub.Context} context - Function context
+ * @return {Promise}
+ */
+async function <%= camelName %>Event(context) {
+  console.log('Pub Sub message: ', { context })
+
+  // End function execution by returning
+  return null
+}
+
+const schedule = 'every 5 minutes'
+
+/**
+ * Cloud Function triggered on a specified CRON schedule
+ *
+ * Trigger: `PubSub - onRun`
+ * @name <%= camelName %>
+ * @type {functions.CloudFunction}
+ */
+export default functions.pubsub.schedule(schedule).onRun(<%= camelName %>Event)<% } %>
